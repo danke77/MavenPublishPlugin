dde661c82a145c131f1d7d15a0d782d56d32286a
danke77 <xue_xiaozhe@163.com>
2018-07-31 10:25:03 +0800

    android sample

---
+++ sample-android/src/main/java/com/danke/android/maven/publish/sample/android/AndroidSample.java
@@ -0,0 +1,7 @@
+package com.danke.android.maven.publish.sample.android;
+
+public class AndroidSample {
+    public static void hello() {
+        System.out.println("Hello world from AndroidSample");
+    }
+}
