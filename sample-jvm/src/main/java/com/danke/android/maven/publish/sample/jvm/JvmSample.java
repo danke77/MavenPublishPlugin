5ac4d4d38f06927bd8cb90d3909f703c841170d9
danke77 <xue_xiaozhe@163.com>
2018-07-31 10:25:35 +0800

    jvm sample

---
+++ sample-jvm/src/main/java/com/danke/android/maven/publish/sample/jvm/JvmSample.java
@@ -0,0 +1,7 @@
+package com.danke.android.maven.publish.sample.jvm;
+
+public class JvmSample {
+    public static void hello() {
+        System.out.println("Hello world from JvmSample");
+    }
+}
